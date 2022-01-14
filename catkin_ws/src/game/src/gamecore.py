6699f733293d5ede9eabe97c5515e10ac53e35ac
rtu <fumincha763@gmail.com>
2022-01-14 14:19:39 +0800

    Modify gamecore

--- catkin_ws/src/game/src/gamecore.py
+++ catkin_ws/src/game/src/gamecore.py
@@ -10,20 +10,29 @@ from sensor_msgs.msg import Image
 from ros_openpose.msg import Frame
 from skeleton.msg import Players
 from geometry_msgs.msg import Pose,PoseArray,PoseStamped
+from game.msg import GameStatus
 
 # cvbridge
 from cv_bridge import CvBridge
 bridge = CvBridge()
 
+game_status = GameStatus()
 PlayerNum=3
-
 names = ['brain','ariel', 'jj'] # add a name into this list
     
 def main():
     global id, goalpub, tfbuf, playerpub
 
-    rospy.init_node('gamecore', anonymous=False)
+    rospy.init_node('GameCore', anonymous=False)
 
+    status_pub = rospy.Publisher('/game/status', GameStatus, queue_size=10)
+    
+    rate = rospy.Rate(10)
+    
+    while not rospy.is_shutdown():
+
+        status_pub.publish()
+        rate.sleep()
     rospy.spin()
 
 if __name__ == '__main__':
